091a80480b2cb3ed53dc4e40b0c88acab1b8b841
0Prashant <073bex425.prashant@pcampus.edu.np>
2018-10-11 19:59:13 +0545

    Navigated with rough encoder data

---
+++ Running codes/motor_tuning/Core/Inc/robot.h
@@ -0,0 +1,18 @@
+/*
+ * robot.h
+ *
+ * Created : 10/3/2018
+ */
+
+#ifndef _ROBOT_H_
+#define _ROBOT_H_
+
+
+
+void run_CurrentConfig(void);
+
+// This function contains an infinite loop
+void play(void);
+int goto_distance(float dx, float dy);
+
+#endif // _ROBOT_H_
